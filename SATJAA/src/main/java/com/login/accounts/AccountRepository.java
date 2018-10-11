aab790da50ffa3dcd7f01c2e689984ce88711b52
s3422910 <42902644+s3422910@users.noreply.github.com>
2018-10-11 23:10:52 +1100

    Create AccountRepository.java

---
+++ SATJAA/src/main/java/com/login/accounts/AccountRepository.java
@@ -0,0 +1,9 @@
+package com.login.accounts;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface AccountRepository extends CrudRepository<Account, String>
+{
+   
+   
+}
