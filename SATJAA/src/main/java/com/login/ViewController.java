46280c77339d0064af2ddee93f051cde0b34a735
s3422910 <42902644+s3422910@users.noreply.github.com>
2018-10-15 14:18:16 +1100

    Update ViewController.java

--- SATJAA/src/main/java/com/login/ViewController.java
+++ SATJAA/src/main/java/com/login/ViewController.java
@@ -1,15 +1,19 @@
 package com.login;
 
+import java.util.Date;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ViewController
 {
    @RequestMapping("/")
-   public String index() {
-      return "index.html";
-      
+   public String index(Model model) {
+      model.addAttribute("datetime", new Date());
+      model.addAttribute("username", "John");
+      return "index";
    }
 
 }
