056f1a3c06fc08cc70620a139e497826e89873fa
s3422910 <42902644+s3422910@users.noreply.github.com>
2018-10-17 19:55:49 +1100

    Create Customer.java

---
+++ SATJAA/src/main/com/app/customers/Customer.java
@@ -0,0 +1,37 @@
+package com.app.customers;
+
+import javax.persistence.Entity;
+
+import com.app.Account;
+
+@Entity
+public class Customer extends Account
+{
+
+   private String DOB;
+   
+   public Customer() {
+      super();
+   }
+   
+   public Customer(Double id, String password, String name, String address, String phone, String DOB)
+   {
+      super.id = id;
+      super.password = password;
+      super.name = name;
+      super.address = address;
+      super.phone = phone;
+      this.DOB = DOB;
+   }
+   
+   public String getDOB()
+   {
+      return DOB;
+   }
+
+   public void setDOB(String DOB)
+   {
+      this.DOB = DOB;
+   }
+   
+}
