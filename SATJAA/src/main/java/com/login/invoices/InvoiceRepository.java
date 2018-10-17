be7a1817fd735e27438977acea9883b912ee794a
s3422910 <42902644+s3422910@users.noreply.github.com>
2018-10-17 19:22:18 +1100

    Update InvoiceRepository.java

--- SATJAA/src/main/java/com/login/invoices/InvoiceRepository.java
+++ SATJAA/src/main/java/com/login/invoices/InvoiceRepository.java
@@ -1,7 +1,5 @@
 package com.login.invoices;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface InvoiceRepository extends JpaRepository<Invoice, Double>
