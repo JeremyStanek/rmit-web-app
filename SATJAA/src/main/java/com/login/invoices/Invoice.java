6435add4e292596a89787d5d5cb0c194f03e4ebf
s3422910 <42902644+s3422910@users.noreply.github.com>
2018-10-17 19:19:54 +1100

    Update Invoice.java

--- SATJAA/src/main/java/com/login/invoices/Invoice.java
+++ SATJAA/src/main/java/com/login/invoices/Invoice.java
@@ -1,4 +1,4 @@
-package com.login.invoices;
+package com.app.invoices;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -6,7 +6,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
-import com.login.accounts.Account;
+import com.app.customers.Customer;
 
 @Entity
 public class Invoice
@@ -15,21 +15,25 @@ public class Invoice
    //@GeneratedValue(strategy = GenerationType.SEQUENCE)
    private Double id;
    
-   private Double totalCost;
+   private Double custId;
+   private double totalCost;
+   private String date;
    
    @ManyToOne
-   private Account account;
+   private Customer customer;
 
    public Invoice()
    {
-  
    }
 
-   public Invoice(Double id, Double totalCost)
+   public Invoice(Double id, Double custId, double totalCost,
+              String date)
    {
       super();
       this.id = id;
+      this.custId = custId;
       this.totalCost = totalCost;
+      this.date = date;
    }
 
    public Double getId()
@@ -37,19 +41,39 @@ public class Invoice
       return id;
    }
 
-   public Double getTotalCost()
-   {
-      return totalCost;
-   }
-
    public void setID(Double id)
    {
       this.id = id;
    }
+   
+   public Double customerId()
+   {
+      return custId;
+   }
+   
+   public void setCustomerId(Double custId)
+   {
+      this.custId = custId;
+   }
+   
+   public double getTotalCost()
+   {
+      return totalCost;
+   }
 
-   public void setTotalCost(Double totalCost)
+   public void setTotalCost(double totalCost)
    {
       this.totalCost = totalCost;
+   }
+   
+   public String getDate()
+   {
+      return date;
+   }
+   
+   public void setDate(String date)
+   {
+      this.date = date;
    }
    
 }
