3c920353ea32b908583fadb4314b1b76343187f3
s3422910 <42902644+s3422910@users.noreply.github.com>
2018-10-28 14:08:32 +1100

    Create EmployerRepository.java

---
+++ SATJAA/src/main/com/app/employers/EmployerRepository.java
@@ -0,0 +1,9 @@
+package com.app.employers;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployerRepository extends JpaRepository<Employer, Double>
+{
+   
+   
+}
